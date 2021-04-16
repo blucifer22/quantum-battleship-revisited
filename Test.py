502db84e23173dfd22d7b4cb5e5e518ae9f16fe7
Juan Lasso Velasco <juanlassovelasco@gmail.com>
2021-04-15 20:57:15 -0400

    added protections against invalid inputs

--- Test.py
+++ Test.py
@@ -1,4 +1,5 @@
 from enum import Enum
+import os
 
 # the current player (either 0 or 1)
 Player = 0
@@ -13,9 +14,9 @@ maxTurns = 4
 turn = 0
 
 class Weapon(Enum):
-    Turret = 20
-    Torpedo = 50
-    Tacticle_Nuke = 200
+    turret = 20
+    torpedo = 50
+    tacticle_nuke = 200
 
 class Board:
     
@@ -70,62 +71,141 @@ board2 = Board()
 
 # initializing weapon
 
+# initialize hiding checkers
+isHiddenP1 = False
+isHiddenP2 = False
+
 # Ship hidding phase
-board1.DrawBoard()
+os.system("clear")
 
-print("Player one, hide your ship")
-hideLocation = input("Choose location to hide: ")
+while isHiddenP1 == False:
+    board1.DrawBoard()
+    print("Player one, hide your ship")
+    hideLocation = input("Choose location to hide: ")
 
-board1.HideShip(hideLocation)
+    if hideLocation.isnumeric():
+        if int(hideLocation) < 5:
+            board1.HideShip(hideLocation)
+            isHiddenP1 = True
+        else:
+            os.system("clear")
+            print("Invalid selection. Selection must be between 1 and 5")
+    else:
+        os.system("clear")
+        print("You must provide a numeric position")
 
+os.system("clear")
 board2.DrawBoard()
 
-print("Player two, hide your ship")
-hideLocation = input("Choose location to hide: ")
+while isHiddenP2 == False:
+    print("Player two, hide your ship")
+    hideLocation = input("Choose location to hide: ")
 
-board2.HideShip(hideLocation)
+    if hideLocation.isnumeric():
+        if int(hideLocation) < 5:
+            board1.HideShip(hideLocation)
+            isHiddenP2 = True
+        else:
+            os.system("clear")
+            print("Invalid selection. Selection must be between 1 and 5")
+    else:
+        os.system("clear")
+        print("You must provide a numeric position")
 
-while(turn < maxTurns):
+# main game loop
+while turn < maxTurns:
+    # initialize input checks
+    hasChosenShotLocationP1 = False
+    hasChosenShotLocationP2 = False
 
-    board2.DrawBoard()
+    hasChosenWeaponP1 = False
+    hasChosenWeaponP2 = False
+
+    os.system("clear")
     print("Current player: " + str(Player))
 
-    shotLocation = input("Choose a location to strike: ")
-    weaponChoice = input("Choose your weapon (weapons: Turret, Torpedo, Tacticle_Nuke): ")
+    while hasChosenShotLocationP1 == False:
+        board2.DrawBoard()
+        shotLocation = input("Choose a location to strike: ")
+
+        # check if input is valid
+        if shotLocation.isnumeric:
+            if int(shotLocation) <= 5:
+                hasChosenShotLocationP1 = True
+            else:
+                os.system("clear")
+                print("Invalid selection. Selection must be between 1 and 5")
+        else:
+            os.system("clear")
+            print("Must give numeric selection")
+
+    while hasChosenWeaponP1 == False:
+        weaponChoice = input("Choose your weapon (weapons: Turret, Torpedo, Tacticle_Nuke): ")
+
+        #check if input is valid
+        if weaponChoice.lower() == "turret":
+            hasChosenWeaponP1 = True
+        elif weaponChoice.lower() == "torpedo":
+            hasChosenWeaponP1 = True
+        elif weaponChoice.lower() == "tacticle_nuke":
+            hasChosenWeaponP1 = True
+        else:
+            print("Must choose a valid weapon")
 
     board2.ChangeBoard(shotLocation)
 
     if board2.mapPos[int(shotLocation) - 1] == 1:
-        if weaponChoice == "Turret":
-            Ship2Health -= Weapon.Turret.value
-        elif weaponChoice == "Torpedo":
-            Ship2Health -= Weapon.Torpedo.value
-        elif weaponChoice == "Tacticle_Nuke":
-            Ship2Health -= Weapon.Tacticle_Nuke.value
-        else:
-            print("you've chosen an invalid weapon. No damage dealt")
+        if weaponChoice == "turret":
+            Ship2Health -= Weapon.turret.value
+        elif weaponChoice == "torpedo":
+            Ship2Health -= Weapon.torpedo.value
+        elif weaponChoice == "tacticle_tuke":
+            Ship2Health -= Weapon.tacticle_nuke.value
     else:
         print("Target missed")
 
     Player = SwitchPlayer(Player)
 
-    board1.DrawBoard()
+    os.system("clear")
     print("Current player: " + str(Player))
 
-    shotLocation = input("Choose a location to strike: ")
-    weaponChoice = input("Choose your weapon (weapons: Turret, Torpedo, Tacticle_Nuke): ")
+    while hasChosenShotLocationP2 == False:
+        board1.DrawBoard()
+        shotLocation = input("Choose a location to strike: ")
+
+        # check if input is valid
+        if shotLocation.isnumeric:
+            if int(shotLocation) <= 5:
+                hasChosenShotLocationP2 = True
+            else:
+                os.system("clear")
+                print("Invalid selection. Selection must be between 1 and 5")
+        else:
+            os.system("clear")
+            print("Must give numeric selection")
+
+    while hasChosenWeaponP2 == False:
+        weaponChoice = input("Choose your weapon (weapons: Turret, Torpedo, Tacticle_Nuke): ")
+
+        #check if input is valid
+        if weaponChoice.lower() == "turret":
+            hasChosenWeaponP2 = True
+        elif weaponChoice.lower() == "torpedo":
+            hasChosenWeaponP2 = True
+        elif weaponChoice.lower() == "tacticle_nuke":
+            hasChosenWeaponP2 = True
+        else:
+            print("Must choose a valid weapon")
 
     board1.ChangeBoard(shotLocation)
 
     if board1.mapPos[int(shotLocation) - 1] == 1:
-        if weaponChoice == "Turret":
-            Ship1Health -= Weapon.Turret.value
-        elif weaponChoice == "Torpedo":
-            Ship1Health -= Weapon.Torpedo.value
-        elif weaponChoice == "Tacticle_Nuke":
-            Ship1Health -= Weapon.Tacticle_Nuke.value
-        else:
-            print("you've chosen an invalid weapon. No damage dealt")
+        if weaponChoice == "turret":
+            Ship1Health -= Weapon.turret.value
+        elif weaponChoice == "torpedo":
+            Ship1Health -= Weapon.torpedo.value
+        elif weaponChoice == "tacticle_nuke":
+            Ship1Health -= Weapon.tacticle_nuke.value
     else:
         print("Target missed")
 
